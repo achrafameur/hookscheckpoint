68644a158bcb98d04b2a7d121628591a3287dcaa
Achraf Ameur <72624148+achrafameur@users.noreply.github.com>
2021-09-06 15:17:15 +0100

    Update MovieList.js

--- src/MovieList.js
+++ src/MovieList.js
@@ -6,7 +6,7 @@ const Movielist = ({list}) => {
     return (
         <Container>
         <Row>
-        {list.length ? list.map( (elm,index)=> <Moviecard key={index} movie={elm}> </Moviecard>) : <h2 style={{color:"white"}}>No Movie Found</h2>}
+        {list.length ?list.map( (elm,index)=><Col md={4} xs={12}> <Moviecard key={index} movie={elm}> </Moviecard></Col>) : <h2 style={{color:"white"}}>No Movie Found</h2>}
         </Row>
         </Container>
     )
